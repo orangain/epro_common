66a3e6e0b6d9ed73507aff61e2a4b79336143836
orangain <orangain@gmail.com>
2013-06-30 16:39:06 +0900

    Initial commit.

---
+++ epro_common/message_queue.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+
+import sys
+import json
+import traceback
+import logging
+
+import pika
+
+logger = logging.getLogger(__name__)
+
+class MessageQueue(object):
+    """
+    pikaを抽象化したメッセージキュー
+    """
+
+    def __init__(self, host, queue, durable=True):
+        self.queue = queue
+
+        logger.info('Connecting to host: %s, queue: %s' % (host, queue))
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host=host))
+        self.channel = connection.channel()
+
+        self.channel.queue_declare(queue=self.queue, durable=durable)
+
+    def consume(self, callback):
+        def _callback(ch, method, properties, body):
+            # とりあえずメッセージボディはJSONにする
+            obj = json.loads(body)
+
+            # callbackでAttributeErrorが発生したときに
+            # 終了しないため、特別にハンドルする
+            try:
+                callback(obj)
+            except AttributeError as ex:
+                traceback.print_exc(file=sys.stderr)
+                raise Exception("AttributeError occured in callback")
+
+            # callbackが正常に終了したらAckを返す
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(_callback, queue=self.queue)
+        self.channel.start_consuming()
+
+    def publish(self, obj):
+        body = json.dumps(obj)
+
+        self.channel.basic_publish(exchange='',
+                routing_key=self.queue,
+                body=body)
